b66abea2d775483e2d18db0bcad0388e0f4de551
Renan Seiji <renanseiji@192.168.0.31>
2026-04-12 15:03:03 -0300

    atualizacao main

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,7 +1,33 @@
-from fastapi import FastAPI
-
+from fastapi import FastAPI, UploadFile, File
+from fastapi.middleware.cors import CORSMiddleware
+import io
+ 
+from pdf_utils import extrair_texto_pdf
+from summarizer import resumir_texto
+ 
 app = FastAPI()
-
-@app.get("/")
-def home():
-    return {"msg": "Hello Docker!"}
+ 
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+ 
+@app.post("/summarize")
+ 
+async def summarize(file: UploadFile = File(...)):
+ 
+    conteudo = await file.read()
+ 
+    pdf_file = io.BytesIO(conteudo)
+ 
+    texto = extrair_texto_pdf(pdf_file)
+ 
+    resumo = resumir_texto(texto)
+ 
+    return {
+        "filename": file.filename,
+        "summary": resumo
+    }
